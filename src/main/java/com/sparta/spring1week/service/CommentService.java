3d1acdb483bf2be91651c8a85cd4a0b9d3568268
Cho-woo-pil <jwp4465@naver.com>
2023-04-21 17:04:17 +0900

    게시글 작성자만 수정 삭제 할수있도록 안해줫었음 수정했음

--- src/main/java/com/sparta/spring1week/service/CommentService.java
+++ src/main/java/com/sparta/spring1week/service/CommentService.java
@@ -1,4 +1,10 @@
 package com.sparta.spring1week.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CommentService {
+
 }
